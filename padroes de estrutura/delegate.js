50a64c090a1f65070a9dfc71a49feab7fca878d0
jokernomade <365codigo@gmail.com>
2022-07-16 15:27:15 -0300

    .

---
+++ padroes de estrutura/delegate.js
@@ -0,0 +1,31 @@
+class Palavra {
+  getStr = () => {
+    throw "Precisa de implementação"
+  }
+}
+
+class Hello extends Palavra {
+  getStr = () => "Hello"
+}
+
+class World extends Palavra {
+  getStr = () => "World"
+}
+
+class Delegador {
+  hello = () => {
+    const hello = new Hello()
+    return hello.getStr()
+  }
+  world = () => {
+    const world = new World()
+    return world.getStr()
+  }
+}
+
+const test = async () => {
+  const delegador = new Delegador()
+  console.log(`${delegador.hello()} ${delegador.world()}`)
+}
+
+(async ()=>{ test() })()
