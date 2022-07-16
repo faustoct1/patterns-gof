f023e062ca1bff462b5160804b683e2068cd9a49
jokernomade <365codigo@gmail.com>
2022-07-16 15:18:28 -0300

    .

---
+++ padroes de estrutura/decorator.js
@@ -0,0 +1,44 @@
+class Palavra {
+  palavra = null
+  constructor(palavra){
+    this.palavra = palavra
+  }
+  getStr = () => { }
+}
+
+class Hello extends Palavra {
+  str = 'Hello'
+  palavra = null
+  constructor(palavra){
+    super(palavra)
+    this.palavra = palavra
+  }
+  getStr = () => { return `${this.str} ${this.palavra ? this.palavra.getStr():''}` }
+}
+
+class World extends Palavra {
+  str = 'World'
+  palavra = null
+  constructor(palavra){
+    super(palavra)
+    this.palavra = palavra
+  }
+  getStr = () => { return `${this.str} ${this.palavra ? this.palavra.getStr():''}` }
+}
+
+class BarraN extends Palavra {
+  str = '\n'
+  palavra = null
+  constructor(palavra){
+    super(palavra)
+    this.palavra = palavra
+  }
+  getStr = () => { return `${this.str} ${this.palavra ? this.palavra.getStr():''}` }
+}
+
+const test = async () => {
+  const helloworld = new BarraN(new Hello(new World(new BarraN())))
+  console.log(helloworld.getStr())
+}
+
+(async ()=>{ test() })()
