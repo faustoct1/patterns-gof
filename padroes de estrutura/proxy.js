279ae44e9a29be0e89f8f16cb4a9f5022a996428
jokernomade <365codigo@gmail.com>
2022-07-17 20:32:15 -0300

    .

---
+++ padroes de estrutura/proxy.js
@@ -0,0 +1,28 @@
+class HelloWorld {
+  getStr = () => "Hello World"
+}
+
+class NaoHelloWorld {
+  getStr2 = () => "Não é Hello World"
+}
+
+class NaoHelloWorldProxy extends HelloWorld {
+  instance = new NaoHelloWorld()
+  getStr = () => this.instance.getStr2()
+}
+
+const proxy = (p) => {
+  console.log(p.getStr())
+}
+
+const test = async () => {
+  proxy(new HelloWorld())
+  try{
+    proxy(new NaoHelloWorld())
+  }catch(e){
+    console.log('Erro essa classe não tem método getStr')
+  }
+  proxy(new NaoHelloWorldProxy())
+}
+
+(async ()=>{ test() })()
