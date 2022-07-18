0ab8ca3187d4713e4a12a5716497fbd95ccd7da5
jokernomade <365codigo@gmail.com>
2022-07-18 15:58:25 -0300

    .

---
+++ padroes de comportamento/visitor.js
@@ -0,0 +1,30 @@
+class Visitante {
+  str = ''
+  visita(obj) { this.str += obj.getStr() }
+  getStr = () => this.str
+}
+
+class HelloVisitado {  
+  aceitaVisitante = (obj) => obj.visita(this)
+  getStr = () => "Hello"
+}
+
+class EspacoVisitado {  
+  aceitaVisitante = (obj) => obj.visita(this)
+  getStr = () => " "
+}
+
+class WorldVisitado {  
+  aceitaVisitante = (obj) => obj.visita(this)
+  getStr = () => "World"
+}
+
+const test = async () => {
+  const visitante = new Visitante()
+  new HelloVisitado().aceitaVisitante(visitante)
+  new EspacoVisitado().aceitaVisitante(visitante)
+  new WorldVisitado().aceitaVisitante(visitante)
+  console.log(visitante.getStr())
+}
+
+(async ()=>{ test() })()
