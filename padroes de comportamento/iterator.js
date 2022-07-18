64d19aea119086026fabca7c74e4dedaad4d45a1
jokernomade <365codigo@gmail.com>
2022-07-18 15:05:39 -0300

    .

---
+++ padroes de comportamento/iterator.js
@@ -0,0 +1,26 @@
+class HelloWorld {
+  str = 'Hello World'.split('')
+  iterador = () => {
+    class Iterador{
+      obj = []
+      index = 0
+      constructor(obj){
+        this.obj = obj
+      }
+      temProximo = () => this.index<this.obj.length
+      proximo = () => this.obj[this.index++]
+    }
+    return new Iterador(this.str)
+  }
+}
+
+const test = async () => {
+  const iterador = (new HelloWorld()).iterador()
+  str = ''
+  while(iterador.temProximo()){
+    str += iterador.proximo()
+  }
+  console.log(str)
+}
+
+(async ()=>{ test() })()
